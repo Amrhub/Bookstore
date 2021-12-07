58ae6b2390f53ca4f80b5a1472348083bfcea318
Amrhub <amr_ahmed65@hotmail.com>
2021-12-07 21:54:36 +0200

    feat: dynamic list

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,72 +1,30 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Books = () => (
-  <main className="books-container">
-    <h1 className="sr-only">Books</h1>
-    <ul className="books-list">
-      <li className="book">
-        <div className="book-info">
-          <span className="book-category">Category</span>
-          <h2 className="book-title">The Lord of the Rings</h2>
-          <p className="book-author">J.R.R. Tolkien</p>
-        </div>
-        <div className="book-crud">
-          <button type="button" className="btn">
-            Comments
-          </button>
-          <button type="button" className="btn">
-            Remove
-          </button>
-          <button type="button" className="btn">
-            Edit
-          </button>
-        </div>
-      </li>
+import AddBook from './AddBook';
+import BookButton from './BookButton';
+import BookInfo from './BookInfo';
 
-      <li className="book">
-        <div className="book-info">
-          <span className="book-category">Category</span>
-          <h2 className="book-title">The Lord of the Rings</h2>
-          <p className="book-author">J.R.R. Tolkien</p>
-        </div>
-        <div className="book-crud">
-          <button type="button" className="btn">
-            Comments
-          </button>
-          <button type="button" className="btn">
-            Remove
-          </button>
-          <button type="button" className="btn">
-            Edit
-          </button>
-        </div>
-      </li>
-    </ul>
+const Books = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <main className="books-container">
+      <h1 className="sr-only">Books</h1>
+      <ul className="books-list">
+        {books.length ? (
+          books.map((book) => (
+            <li className="book" data-id={book.id} key={book.id}>
+              <BookInfo title={book.title} author={book.author} />
+              <BookButton />
+            </li>
+          ))
+        ) : (
+          <p>No books to display, please add one </p>
+        )}
+      </ul>
 
-    <form className="book-form">
-      <label htmlFor="book-title" className="sr-only">
-        Title
-      </label>
-      <input
-        type="text"
-        className="form-control"
-        id="book-title"
-        placeholder="Title"
-      />
-      <label htmlFor="book-category" className="sr-only">
-        Category
-      </label>
-      <select className="form-control" id="book-category">
-        <option>Category</option>
-        <option>Category 1</option>
-        <option>Category 2</option>
-      </select>
-      <button type="submit" className="btn-primary btn-submit">
-        add book
-      </button>
-    </form>
-  </main>
-);
-
+      <AddBook />
+    </main>
+  );
+};
 export default Books;
