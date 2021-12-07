64625ebe982e095bbcf6e36fafd75ad3da159363
Amrhub <amr_ahmed65@hotmail.com>
2021-12-07 21:52:49 +0200

    feat: wrap provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
