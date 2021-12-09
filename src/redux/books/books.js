942f542ca85601c7bdea23e50908fb3b474aafb9
Amrhub <amr_ahmed65@hotmail.com>
2021-12-09 12:40:41 +0200

    feat: create instance from api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const APP_ID = 'R8J1j82iESbbKkWiu7Ig';
+const APIURL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}/books`;
 
 const initialState = [];
 
